e4ffd7943226c7202de5c5685cee8d919b0fec33
EdwardSpring <eddistics@gmail.com>
2023-11-03 15:58:21 +0100

    Test transaction factory

--- src/test/java/com/algomeri/factory/TransactionFactoryTest.java
+++ src/test/java/com/algomeri/factory/TransactionFactoryTest.java
@@ -1,3 +1,28 @@
 package com.algomeri.factory;
 
-public class TransactionFactoryTest {}
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.algomeri.sdk.enumeration.PaymentPlatform;
+import com.algomeri.service.Transaction;
+import com.algomeri.service.impl.paystack.PaystackTransaction;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TransactionFactoryTest {
+
+    @Test
+    public void testGetInstance() throws Exception {
+        for (PaymentPlatform platform : PaymentPlatform.values()) {
+            switch (platform) {
+                case PAYSTACK:
+                    Transaction transaction = TransactionFactory.getInstance(platform);
+                    Assertions.assertThat(transaction).isInstanceOf(PaystackTransaction.class);
+                    break;
+                default:
+            }
+        }
+    }
+}
