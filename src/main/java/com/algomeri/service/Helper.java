9fc9c5692c112d594ba6fd3a9575c2958980bf76
edwardspring <eddistics@gmail.com>
2024-02-17 14:27:27 +0100

    added docs to helper

--- src/main/java/com/algomeri/service/Helper.java
+++ src/main/java/com/algomeri/service/Helper.java
@@ -1,6 +1,5 @@
 package com.algomeri.service;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 import com.algomeri.data.AccountVerifier;
@@ -42,12 +41,12 @@ public interface Helper {
 
     /**
      * This is useful when you're trying to make sure you get your full sum after settlement.
-     * Example, your charge is 5000 and gateway fee is 1.5%.
+     * <p>Example, your charge is 5000 and gateway fee is 1.5%.
      * This returns an estimated amout you should charge your customer in order to still
      * receive 5000 after gateway fees have been deducted.
      * This is only applicable to Nigerian businesses if implementing for flutterwave
      * @param amount -> the actual amount you want to receive after user pays
-     * @param isLocalPayment
+     * @param isLocalPayment -> defaults to true if null
      * @return {@link Estimate}
      */
     public Estimate estimateFee(double amount, Boolean isLocalPayment);
